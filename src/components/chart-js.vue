431281f4875dde7f3bfaa19a4639fa305653abc0
Jorge Luis Veliz <jorge.veliz@guayas.gob.ec>
2020-01-04 13:33:51 -0500

    chartjs

--- src/components/chart-js.vue
+++ src/components/chart-js.vue
@@ -4,6 +4,8 @@
 
 <script>
 import ChartJs from "chart.js";
+const axios = require("axios");
+const DOMAIN = "https://jsonplaceholder.typicode.com/";
 
 export default {
   data() {
@@ -11,48 +13,57 @@ export default {
   },
   methods: {
     loadData() {
-      let div = document.getElementById("chartjs");
-      var chartJs = new ChartJs(div, {
-        type: "bar",
-        data: {
-          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-          datasets: [
-            {
-              label: "# of Votes",
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                "rgba(255, 99, 132, 0.2)",
-                "rgba(54, 162, 235, 0.2)",
-                "rgba(255, 206, 86, 0.2)",
-                "rgba(75, 192, 192, 0.2)",
-                "rgba(153, 102, 255, 0.2)",
-                "rgba(255, 159, 64, 0.2)"
-              ],
-              borderColor: [
-                "rgba(255, 99, 132, 1)",
-                "rgba(54, 162, 235, 1)",
-                "rgba(255, 206, 86, 1)",
-                "rgba(75, 192, 192, 1)",
-                "rgba(153, 102, 255, 1)",
-                "rgba(255, 159, 64, 1)"
-              ],
-              borderWidth: 1
+      axios.get(DOMAIN + "todos").then(result => {
+        let data = [];
+
+        for (let index = 0; index < result.data.length; index++) {
+          const element = result.data[index];
+          if (element.completed) {
+            if (data[0]) {
+              data[0] += 1;
+            } else {
+              data[0] = 1;
             }
-          ]
-        },
-        options: {
-          scales: {
-            yAxes: [
-              {
-                ticks: {
-                  beginAtZero: true
-                }
-              }
-            ]
+          } else {
+            if (data[1]) {
+              data[1] += 1;
+            } else {
+              data[1] = 1;
+            }
           }
         }
+
+        let div = document.getElementById("chartjs");
+        var chartJs = new ChartJs(div, {
+          type: "bar",
+          data: {
+            labels: ["Completos", "Incompletos"],
+            datasets: [
+              {
+                label: "# de tareas",
+                data: data,
+                backgroundColor: [
+                  "rgba(255, 99, 132, 0.2)",
+                  "rgba(54, 162, 235, 0.2)"
+                ],
+                borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)"],
+                borderWidth: 1
+              }
+            ]
+          },
+          options: {
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    beginAtZero: true
+                  }
+                }
+              ]
+            }
+          }
+        });
       });
-      console.log(chartJs);
     }
   },
   mounted() {
